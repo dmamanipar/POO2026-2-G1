85d5c0c7ff1032b194c60c37eaf8d2f6bfb7b50c
dmamanipar <dmamanipar@unsa.edu.pe>
2026-09-14 10:22:17 -0500

    Avance P2

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/service/impl/CategoriaServiceImp.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/service/impl/CategoriaServiceImp.java
@@ -1,9 +1,14 @@
 package pe.edu.upeu.sysventas.service.impl;
 
+import pe.edu.upeu.sysventas.dto.ComboBoxOption;
+import pe.edu.upeu.sysventas.enums.TipoProducto;
 import pe.edu.upeu.sysventas.model.Categoria;
 import pe.edu.upeu.sysventas.repository.CategoriaRepository;
 import pe.edu.upeu.sysventas.repository.ICrudGenericoRepository;
 import pe.edu.upeu.sysventas.service.ICategoriaService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriaServiceImp extends CrudGenericoServiceImp<Categoria, Long> implements ICategoriaService {
 
@@ -18,4 +23,15 @@ public class CategoriaServiceImp extends CrudGenericoServiceImp<Categoria, Long>
         return categoriaRepository;
     }
 
+    @Override
+    public List<ComboBoxOption> lisCategoria() {
+        List<ComboBoxOption> listar=new ArrayList<>();
+        for (Categoria cat:categoriaRepository.findAll()){
+            ComboBoxOption cb=new ComboBoxOption();
+            cb.setKey(String.valueOf(cat.getIdCategoria()));
+            cb.setValue(cat.getNombre());
+            listar.add(cb);
+        }
+        return listar;
+    }
 }
